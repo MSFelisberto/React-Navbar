bf2002f9374f03aabdd065c0475c779a9852e524
Marcos Felisberto <marcossfelisberto@gmail.com>
2021-09-30 15:34:13 -0300

    Initial commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Inicio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Serviços',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Produtos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contato',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Cadastre-se',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
